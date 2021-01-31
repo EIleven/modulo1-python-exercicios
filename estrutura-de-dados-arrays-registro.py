bf3920a165e3d2a2fe796f27caa02bc8f0e08cee
Breno César <44047399+EIleven@users.noreply.github.com>
2021-01-31 01:10:51 -0300

    Create estrutura-de-dados-arrays-registro.py

---
+++ estrutura-de-dados-arrays-registro.py
@@ -0,0 +1,6 @@
+# Estruturas de Registro:
+print('Campos de Registro de um cliente em um Sistema')
+cpf = int(input('Digite seu CPF: '))
+nome = str(input('Digite seu nome completo: ')).strip()
+endereco = all(input('Digite seu endereço: '))
+contato = all(input('Digite seu número de contato: '))
